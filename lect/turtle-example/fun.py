4825acefdc9c769fea566508ab489a6c59ec56dd
Jason Shepherd <jbshep@gmail.com>
2021-09-14 14:34:07 -0500

    turtles: fun.py

--- lect/turtle-example/fun.py
+++ lect/turtle-example/fun.py
@@ -1,6 +1,16 @@
+from turtles import *
+
 # Stack some turtles.
 # Write a loop that shows the contents of the turtle stack.
 
-Timmy
-Maximus
-Maximusser
+c1 = Turtle('Timmy')
+c2 = ColossusTurtle('Maximus', c1)
+c3 = ColossusTurtle('Maximusser', c2)
+
+t = c3
+while t:
+    print(t.name)
+    if type(t) == ColossusTurtle:
+        t = t.buddy
+    else:
+        t = None
